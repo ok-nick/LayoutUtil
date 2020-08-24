bd6dabd61c00dff9be479e681a592c3523fa595a
Nickuhhh <nickyboy04@icloud.com>
2020-08-24 18:40:10 -0400

    create rather than new

--- LayoutScaler/GridLayout.lua
+++ LayoutScaler/GridLayout.lua
@@ -16,13 +16,13 @@ end
 
 function Grid:ResizeContent() -- https://devforum.roblox.com/t/dynamically-sized-scrolling-frame-with-scale/210748/3?u=iinemo
 	local AbsoluteSize = self.ScrollingFrame.AbsoluteSize
-	
+
 	local NewPadding = self.Padding * AbsoluteSize
 	local NewSize = self.Size * AbsoluteSize
 
 	self.Layout.CellPadding = UDim2.new(0, NewPadding.X, 0, NewPadding.Y)
 	self.Layout.CellSize = UDim2.new(0, NewSize.X, 0, NewSize.Y)
-	
+
 	self:ResizeCanvas()
 end
 
@@ -30,15 +30,15 @@ end
 function Grid:Bind()
 	if self.Binded then return end
 	self.Binded = true
-	
+
 	self.OnResize = self.ScrollingFrame:GetPropertyChangedSignal('AbsoluteSize'):Connect(function()
 		self:ResizeContent()
 	end)
-	
+
 	self.ChildRemoved = self.ScrollingFrame.ChildRemoved:Connect(function()
 		self:ResizeCanvas()
 	end)
-	
+
 	self.ChildAdded = self.ScrollingFrame.ChildAdded:Connect(function()
 		self:ResizeCanvas()
 	end)
@@ -48,7 +48,7 @@ end
 function Grid:Unbind()
 	if not self.Binded then return end
 	self.Binded = false
-	
+
 	self.OnResize:Disconnect()
 	self.ChildAdded:Disconnect()
 	self.ChildRemoved:Disconnect()
@@ -59,7 +59,7 @@ function Grid:SetDefault(Padding, Size)
 	local function ToScale(Absolute, Axis)
 		return (Absolute[Axis].Offset / self.ScrollingFrame.AbsoluteSize[Axis]) + Absolute[Axis].Scale
 	end
-	
+
 	self.Padding = Vector2.new(ToScale(Padding, 'X'), ToScale(Padding, 'Y'))
 	self.Size = Vector2.new(ToScale(Size, 'X'), ToScale(Size, 'Y'))
 end
@@ -68,16 +68,16 @@ end
 -- Library --
 
 
-function Grid.new(Layout)
+function Grid.create(Layout)
 	local self = setmetatable({}, Grid)
-	
+
 	self.ScrollingFrame = Layout.Parent
 	self.Layout = Layout
-	
+
 	self:SetDefault(Layout.CellPadding, Layout.CellSize)
 	self:ResizeContent()
 	self:Bind()
-	
+
 	return self
 end
 
--- LayoutScaler/ListLayout.lua
+++ LayoutScaler/ListLayout.lua
@@ -33,13 +33,13 @@ end
 function List:ResizeContent()
 	for i = 1, #self.Children do
 		local Info = self.Children[i]
-		
+
 		local NewSize = Info.DefaultSize * self.ScrollingFrame.AbsoluteSize
 		Info.Object.Size = UDim2.new(0, NewSize.X, 0, NewSize.Y)
 	end
-	
+
 	self.Layout.Padding = UDim.new(0, self.Padding * self.ScrollingFrame.AbsoluteSize[self.Axis])
-	
+
 	self:ResizeCanvas()
 end
 
@@ -47,25 +47,25 @@ end
 function List:Bind()
 	if self.Binded then return end
 	self.Binded = true
-	
+
 	self.OnResize = self.ScrollingFrame:GetPropertyChangedSignal('AbsoluteSize'):Connect(function()
 		self:ResizeContent()
 	end)
-	
+
 	self.AxisChanged = self.Layout:GetPropertyChangedSignal('FillDirection'):Connect(function()
 		self.Axis = GetAxis(self.Layout.FillDirection)
 		self:ResizeContent()
 	end)
-	
+
 	self.ChildRemoved = self.ScrollingFrame.ChildRemoved:Connect(function(Object)
 		local Index = table.find(self.Children, Object)
 		if Index then
 			table.remove(self.Children, Index)
 		end
-		
+
 		self:ResizeCanvas()
 	end)
-	
+
 	self.ChildAdded = self.ScrollingFrame.ChildAdded:Connect(function(Object)
 		self:AddObject(Object)
 		self:ResizeCanvas()
@@ -76,7 +76,7 @@ end
 function List:Unbind()
 	if not self.Binded then return end
 	self.Binded = false
-	
+
 	self.OnResize:Disconnect()
 	self.ChildAdded:Disconnect()
 	self.AxisChanged:Disconnect()
@@ -92,22 +92,22 @@ end
 -- Library --
 
 
-function List.new(Layout)
+function List.create(Layout)
 	local self = setmetatable({}, List)
-	
+
 	self.Axis = GetAxis(Layout.FillDirection)
 	self.ScrollingFrame = Layout.Parent
 	self.Layout = Layout
 	self.Children = {}
-	
+
 	for _, Object in next, self.ScrollingFrame:GetChildren() do
 		self:AddObject(Object)
 	end
-	
+
 	self:SetDefault(Layout.Padding)
 	self:ResizeContent()
 	self:Bind()
-	
+
 	return self
 end
 
