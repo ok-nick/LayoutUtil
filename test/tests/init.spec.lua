5f3ae7386bc8da3f33eafc7dcac93c9f8cfeab60
ok-nick <25470747+ok-nick@users.noreply.github.com>
2022-07-19 19:06:09 -0400

    format

--- test/tests/init.spec.lua
+++ test/tests/init.spec.lua
@@ -1,15 +1,15 @@
 return function()
-	local LayoutUtil = require(game:GetService('ReplicatedStorage').LayoutUtil)
+	local LayoutUtil = require(game:GetService("ReplicatedStorage").LayoutUtil)
 
-	it('should insert a UIAspectRatioConstraint and calculate the AspectRatio', function()
-		local object = Instance.new('Frame')
+	it("should insert a UIAspectRatioConstraint and calculate the AspectRatio", function()
+		local object = Instance.new("Frame")
 		LayoutUtil.constraint(object, Vector2.new(100, 50))
 
 		expect(object.UIAspectRatioConstraint.AspectRatio).to.equal(2)
 	end)
 
-	it('should constraint the UIGridLayout', function()
-		local layout = Instance.new('UIGridLayout')
+	it("should constraint the UIGridLayout", function()
+		local layout = Instance.new("UIGridLayout")
 		layout.CellSize = UDim2.fromOffset(100, 50)
 		LayoutUtil.grid(layout, Vector2.new(200, 10))
 
@@ -17,11 +17,11 @@ return function()
 	end)
 
 	it("should constraint each child of the UIListLayout's parent", function()
-		local frame = Instance.new('Frame')
-		local child = Instance.new('Frame')
+		local frame = Instance.new("Frame")
+		local child = Instance.new("Frame")
 		child.Size = UDim2.fromOffset(100, 50)
 		child.Parent = frame
-		local layout = Instance.new('UIListLayout')
+		local layout = Instance.new("UIListLayout")
 		layout.Parent = frame
 
 		LayoutUtil.list(layout)
@@ -30,13 +30,13 @@ return function()
 	end)
 
 	it("should automatically constraint each new child of the UIListLayout's parent", function()
-		local frame = Instance.new('Frame')
-		local layout = Instance.new('UIListLayout')
+		local frame = Instance.new("Frame")
+		local layout = Instance.new("UIListLayout")
 		layout.Parent = frame
 
 		LayoutUtil.watch(layout)
 
-		local child = Instance.new('Frame')
+		local child = Instance.new("Frame")
 		child.Size = UDim2.fromOffset(100, 50)
 		child.Parent = frame
 
